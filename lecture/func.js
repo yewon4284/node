fbf4b3d9b86e662648f0135e6094568d6c78e6a1
yewon4284 <142921713+yewon4284@users.noreply.github.com>
2023-09-26 22:13:51 +0900

    first

---
+++ lecture/func.js
@@ -0,0 +1,11 @@
+const { odd, even } = require('./var');
+
+function checkOddOrEven(number) {
+    if (number % 2) {
+        return odd;
+    } else {
+        return even;
+    }
+}
+
+module.exports = checkOddOrEven;
---
+++ lecture/var.js
@@ -0,0 +1,7 @@
+const odd = '홀수';
+const even = '짝수';
+
+exports.odd = odd;
+exports.even = even;
+
+
