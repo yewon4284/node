622f4240fdbaff4cad793c5b3a6c9fd350b7f09b
yewon4284 <142921713+yewon4284@users.noreply.github.com>
2023-10-13 22:26:11 +0900

    server2

--- lecture/server1.js
+++ lecture/server1.js
@@ -1,10 +1,23 @@
 const http = require('http');
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     res.write('<h1>Hello Node!</h1>');
     res.write('<p>Hello server</p>');
     res.end('<p>Hello Zerocho</p>');
 })
-    .listen(8080, () => {
+    .listen(8080);
+server.on('listening', () => {
         console.log('8080번 포트에서 서버 대기 중');
-    });
+});
+server.on('error', (error) => {
+    console.error(error);
+});
+
+const server1 = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write('<h1>Hello Node!</h1>');
+    res.write('<p>Hello server</p>');
+    res.end('<p>Hello Zerocho</p>');
+})
+    .listen(8081);
---
+++ lecture/server2.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+const server = http.createServer(async(req, res) => {
+    try {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        const data = await fs.readFile('./server2.html');
+        res.end(data);
+    } catch (error) {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.end(err.message);
+    }
+})
+    .listen(8080);
+server.on('listening', () => {
+        console.log('8080번 포트에서 서버 대기 중');
+});
+server.on('error', (error) => {
+    console.error(error);
+});
