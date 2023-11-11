0872555b69a50ab94ff5bb0f7d83df6a26e89f9d
yewon4284 <142921713+yewon4284@users.noreply.github.com>
2023-11-11 21:09:39 +0900

    sequelize

---
+++ lecture/models/index.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const User = require('./user');
+const Comment = require('./comment');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.seqelize = sequelize;
+db.Seqelize = Sequelize;
+
+module.exports = db;
