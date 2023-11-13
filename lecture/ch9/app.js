6b8ccd1d131fd71ee33779a3ae9580d9a99d4759
yewon4284 <142921713+yewon4284@users.noreply.github.com>
2023-11-13 20:31:17 +0900

    ch9.1

---
+++ lecture/ch9/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require = require('dotenv');
+
+dotenv.config();
+const pageRouter = require('./routes/page');
