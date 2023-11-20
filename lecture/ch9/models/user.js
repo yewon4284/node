924ef3b7c8460b8e5f21e9bdab24b8a90674ab81
yewon4284 <142921713+yewon4284@users.noreply.github.com>
2023-11-20 17:42:20 +0900

    9.3

--- lecture/ch9/models/user.js
+++ lecture/ch9/models/user.js
@@ -1,55 +1,55 @@
 const Sequelize = require('sequelize');
 
 class User extends Sequelize.Model {
-    static initiate(sequelize) {
-        User.init( {
-            email: {
-                type: Sequelize.STRING(40),
-                allowNull: true,
-                unique: true,
-            },
-            nick: {
-                type: Sequelize.STRING(15),
-                allowNull: false,
-            },
-            password: {
-                type: Sequelize.STRING(100),
-                allowNull: true,
-            },
-            provider: {
-                type: Sequelize.ENUM('local', 'kakao'),
-                allowNull: false,
-                defuaultValue: 'local'
-            },
-            snsId: {
-                type: Sequelize.STRING(30),
-                allowNull: true,
-            }
-        }, {
-            sequelize,
-            timestamps: true, // createAt, updatedAt
-            underscored: false,
-            modeName: 'User',
-            tableName: 'users',
-            paranoid: true, // deleteAt 삭제일 // soft delete
-            charset: 'utf8',
-            collate: 'utf8_general_ci',
-        });
-    }
-
-    static associate(db) {
-        db.User.hasMany(db.Post);
-        db.User.belongsToMany(db.User, { // 팔로워 //다대다(테이블이 생김)
-          foreignKey: 'followingId',
-          as: 'Followers', 
-          through: 'Follow',
+  static initiate(sequelize) {
+    User.init({
+      email: {
+        type: Sequelize.STRING(40),
+        allowNull: true,
+        unique: true,
+      },
+      nick: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: true,
+      },
+      provider: {
+        type: Sequelize.ENUM('local', 'kakao'),
+        allowNull: false,
+        defaultValue: 'local',
+      },
+      snsId: {
+        type: Sequelize.STRING(30),
+        allowNull: true,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
     });
-    db.User.belongsToMany(db.User, { // 팔로잉 // 다대다
-      foreignKey: 'followerId',
-      as: 'Followings', 
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Post);
+    db.User.belongsToMany(db.User, {
+      foreignKey: 'followingId',
+      as: 'Followers',
       through: 'Follow',
     });
-    }
+    db.User.belongsToMany(db.User, {
+      foreignKey: 'followerId',
+      as: 'Followings',
+      through: 'Follow',
+    });
+  }
 };
 
 module.exports = User;
